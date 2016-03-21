knubie/Fortress

commit f21812e4e07a46c12260c9393d9d3dbd959ea66b

Prevent didFindMatch from loading twice
Addresses #7

Loading a match from the GKMatchMakerView will fire the didFindMatch
event as well as the receivedTurnEventForMatch event. Previously they
would be called at the same time, causing the match loading to take
twice as long.

However this solution introduces a bug where loading a match from a
notification no longer works.

ios/GameCenterManager.h
@@ -13,5 +13,6 @@
 @interface GameCenterManager : UIViewController <RCTBridgeModule, GKLocalPlayerListener, GKTurnBasedMatchmakerViewControllerDelegate>
 
 @property GKTurnBasedMatch *currentMatch;
+@property BOOL GKMatchmakerViewControllerActive;
 
 @end

ios/GameCenterManager.m
@@ -108,10 +108,12 @@ RCT_EXPORT_METHOD(newMatch)
 //  mmvc.showExistingMatches = NO;
   
   [rootController presentViewController:mmvc animated:YES completion:nil];
+  self.GKMatchmakerViewControllerActive = YES;
 }
 
 RCT_EXPORT_METHOD(clearMatch)
 {
+  NSLog(@"Clear match");
   self.currentMatch = nil;
 }
 
@@ -187,7 +189,7 @@ RCT_EXPORT_METHOD(endTurnWithNextParticipants:(NSString *)game)
   }
 }
 
-// Called when loading a match from GKTurnBasedMatchMaker
+// Called when loading a match from GKTurnBasedMatchMaker - did become active
 // Called when the app is open, and the other player updates the match - did not become active
 // Called when the app is opened from notification - did become active
 - (void)player:(GKPlayer *)player receivedTurnEventForMatch:(GKTurnBasedMatch *)match didBecomeActive:(BOOL)didBecomeActive
@@ -200,7 +202,13 @@ RCT_EXPORT_METHOD(endTurnWithNextParticipants:(NSString *)game)
     NSLog(@"received turn event - did become active");
     // Launch the playView from didfindmatch
     self.currentMatch = match;
-    [_bridge.eventDispatcher sendAppEventWithName:@"didFindMatch" body:event]; // Home event
+    // TODO: Add event for loading match from notification
+    if (self.GKMatchmakerViewControllerActive) {
+      [_bridge.eventDispatcher sendAppEventWithName:@"didFindMatch" body:event]; // Home event
+      UIViewController *rootController = (UIViewController*)[[(AppDelegate*)
+                                                              [[UIApplication sharedApplication]delegate] window] rootViewController];
+      [rootController dismissViewControllerAnimated:YES completion:nil];
+    }
     if (match.matchData.length != 0) {
       [_bridge.eventDispatcher sendAppEventWithName:@"updateMatchData" body:event]; // PlayView event
     }
@@ -276,6 +284,10 @@ RCT_EXPORT_METHOD(endTurnWithNextParticipants:(NSString *)game)
   [_bridge.eventDispatcher sendAppEventWithName:@"didFindMatch" body:event];
 
   [rootController dismissViewControllerAnimated:YES completion:nil];
+  
+  self.GKMatchmakerViewControllerActive = NO;
+  
+  NSLog(@"End of didFindMatch");
   
 //  [match removeWithCompletionHandler:^(NSError *error) {
 //    

back-ends/game-center.js
@@ -79,7 +79,6 @@ var compressGame = R.evolve({
 
 //  instantiateObjects :: (Object) -> Game
 var instantiateObjects = R.compose(Types.Game.of, R.evolve({
-  plys: R.always([]),
   board: R.compose(Types.Board.of, R.evolve({
     pieces: R.map(R.compose(Types.Piece.of, R.evolve({
       position: Types.Position.of

commit c6524dd95e9e7084d96eb22a2e5d18b8969b0a9b

Fix issue where matches weren't loaded from PN
This issue was introduced in f21812e, addresses #7

The code now does two checks. First it checks if the deprecated
didFindMatch event was fired, then checks if the
GKTurnBasedMatchmakerViewController is active, and responds accordingly.
This solution handles the now deprecated didFindMatch event on older
devices as well as newer devices that may remove the event all together.

ios/GameCenterManager.h
@@ -14,5 +14,6 @@
 
 @property GKTurnBasedMatch *currentMatch;
 @property BOOL GKMatchmakerViewControllerActive;
+@property BOOL didFindMatch;
 
 @end

ios/GameCenterManager.m
@@ -203,8 +203,13 @@ RCT_EXPORT_METHOD(endTurnWithNextParticipants:(NSString *)game)
     // Launch the playView from didfindmatch
     self.currentMatch = match;
     // TODO: Add event for loading match from notification
-    if (self.GKMatchmakerViewControllerActive) {
+    if (!self.didFindMatch) {
+      self.didFindMatch = YES;
       [_bridge.eventDispatcher sendAppEventWithName:@"didFindMatch" body:event]; // Home event
+    } else {
+      self.didFindMatch = NO;
+    }
+    if (self.GKMatchmakerViewControllerActive) {
       UIViewController *rootController = (UIViewController*)[[(AppDelegate*)
                                                               [[UIApplication sharedApplication]delegate] window] rootViewController];
       [rootController dismissViewControllerAnimated:YES completion:nil];
@@ -274,6 +279,8 @@ RCT_EXPORT_METHOD(endTurnWithNextParticipants:(NSString *)game)
 - (void)turnBasedMatchmakerViewController:(GKTurnBasedMatchmakerViewController *)viewController didFindMatch:(GKTurnBasedMatch *)match
 {
   NSLog(@"didFindMatch");
+  
+  self.didFindMatch = YES;
   
   UIViewController *rootController = (UIViewController*)[[(AppDelegate*)
                                                           [[UIApplication sharedApplication]delegate] window] rootViewController];